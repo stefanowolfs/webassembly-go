268f6b1ab60a7832749373b1cc0da51b34ccdfa0
Stefano Soro <stefano.soro@sap.com>
2023-09-27 23:33:38 -0300

    :sparkles: Add error handling

--- cmd/wasm/wasm.go
+++ cmd/wasm/wasm.go
@@ -19,29 +19,41 @@ func prettyJson(input string) (string, error) {
 }
 
 func jsonWrapper() js.Func {
-	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
+	jsonfunc := js.FuncOf(func(this js.Value, args []js.Value) any {
 		if len(args) != 1 {
-			return "Invalid no of arguments passed"
+			result := map[string]any{
+				"error": "Invalid no of arguments passed",
+			}
+			return result
 		}
 		jsDoc := js.Global().Get("document")
 		if !jsDoc.Truthy() {
-			return "Unable to get document object"
+			result := map[string]any{
+				"error": "Unable to get document object",
+			}
+			return result
 		}
 		jsonOuputTextArea := jsDoc.Call("getElementById", "jsonoutput")
 		if !jsonOuputTextArea.Truthy() {
-			return "Unable to get output text area"
+			result := map[string]any{
+				"error": "Unable to get output text area",
+			}
+			return result
 		}
 		inputJSON := args[0].String()
 		fmt.Printf("input %s\n", inputJSON)
 		pretty, err := prettyJson(inputJSON)
 		if err != nil {
 			errStr := fmt.Sprintf("unable to parse JSON. Error %s occurred\n", err)
-			return errStr
+			result := map[string]any{
+				"error": errStr,
+			}
+			return result
 		}
 		jsonOuputTextArea.Set("value", pretty)
 		return nil
 	})
-	return jsonFunc
+	return jsonfunc
 }
 
 func main() {
